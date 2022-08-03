68c25b7662cdf5fa2fc3d05fc881bf9b9ded0342
Sa2 <ar-ciel_ar-dor_harvestasya@live.jp>
2022-08-03 22:15:46 +0900

    add zig by brew install

--- scripts/install-brew-component.sh
+++ scripts/install-brew-component.sh
@@ -7,7 +7,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew update && brew upgrade
 brew tap jhawthorn/fzy
-brew install git bash vim tmux python goenv z bash-completion gsed peco reattach-to-user-namespace tfenv nodebrew
+brew install git bash vim tmux python goenv zig z bash-completion gsed peco reattach-to-user-namespace tfenv nodebrew
 
 brew install --cask alacritty chrome
 
