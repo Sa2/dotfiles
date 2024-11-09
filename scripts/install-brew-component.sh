3c370caa4a7cdeae0eecea040c713f46f7cdc5a6
Sa2 <ar-ciel_ar-dor_harvestasya@live.jp>
2024-11-09 20:22:57 +0900

    add colima

--- scripts/install-brew-component.sh
+++ scripts/install-brew-component.sh
@@ -7,7 +7,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew update && brew upgrade
 brew tap jhawthorn/fzy
-brew install git bash vim nvim tmux python goenv zig z bash-completion gsed peco reattach-to-user-namespace tfenv nvm ykman netcat zed
+brew install git bash vim nvim tmux python goenv zig z bash-completion gsed peco reattach-to-user-namespace tfenv nvm ykman netcat zed colima docker docker-compose
 
 brew install --cask alacritty chrome dbeaver-community
 
